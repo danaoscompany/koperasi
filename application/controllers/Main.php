c0f9fbc887dd4cef465d45be38180c7633814d2c
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-03-01 05:27:28 +0700

    a

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -426,7 +426,7 @@ public function get_saldo_tersedia() {
 			    );
 			    echo json_encode($response);
 			} else {
-			    if (password_verify("abc", $user['password'])) {
+			    if (password_verify($password, $user['password'])) {
 			        if (intval($user['active']) != 1) {
 			            $response = array(
 				        "id" => intval($user["id"]),
