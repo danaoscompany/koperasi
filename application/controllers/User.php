232993b31545cdcc0fe23a01bb8a0baa04730d21
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-03-06 23:32:33 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -242,6 +242,7 @@ $this->upload->initialize($config);
       $noAnggota = $this->db->get_where('nasabah', array(
       	'user_id' => $userID
       ))->row_array()['no_anggota'];
+      $this->db->order_by('synced_date', 'desc');
       $query = $this->db->get_where('riwayat_simpanan', array(
           'no_anggota' => $noAnggota
       ))->result_array();
