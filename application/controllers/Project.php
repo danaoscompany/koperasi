e9ecb338c9297ec50a789b98cfcc0116a6eaccbf
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-03-07 17:14:03 +0700

    a

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -8,6 +8,20 @@ class Project extends CI_Controller {
 	  return $value;
 	}
 	
+	public function get_porsi_modal() {
+		$kodeProject = $this->get_post_value('kode_project');
+		$userID = intval($this->get_post_value('user_id'));
+		$query = $this->db->get_where('riwayat_simpanan', array(
+			'kode_project' => $kodeProject,
+			'user_id' => $userID
+		))->result_array();
+		$total = 0;
+		for ($i=0; $i<sizeof($query); $i++) {
+			$total += intval($query[$i]['debet']);
+		}
+		echo $total;
+	}
+	
 	public function get_nilai_project() {
 		$kodeProject = $this->get_post_value('kode_project');
 		$query = $this->db->get_where('riwayat_simpanan', array(
